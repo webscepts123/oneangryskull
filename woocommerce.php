b289b22afae18a68d46643b84bcb366b9eb5b294
Dinudu <dinudu@digixnet.com>
2023-11-13 09:30:47 +0530

    modify

--- woocommerce.php
+++ woocommerce.php
@@ -15,23 +15,15 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="container"> 
+ 
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+     
+     
+ <?php woocommerce_content(); ?>
 
-			get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+</div>
 
 <?php
 get_sidebar();
