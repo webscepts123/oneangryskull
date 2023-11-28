48be82ae5c861d29559f56a9c61818d79ef1259b
Dinudu <dinudu@digixnet.com>
2023-11-28 11:53:33 +0530

    header

--- footer.php
+++ footer.php
@@ -32,17 +32,17 @@
             <!--footer links-->
             <div class="footer-links">
                 <div class="row">
-                    <div class="col-sm-4 col-md-2">
+                    <div class="col-sm-4 col-md-3">
                         <h5>Browse by</h5>
                         <ul>
-                            <li><a href="#">Brand</a></li>
-                            <li><a href="#">Product</a></li>
-                            <li><a href="#">Category</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Sales</a></li>
+                            <li><a href="#">Men</a></li>
+                            <li><a href="#">Woman</a></li>
+                            <li><a href="#">Accessories</a></li>
+                            <li><a href="#">Sportwear</a></li>
+                            <li><a href="#">FAQ</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-4 col-md-2">
+                    <!--<div class="col-sm-4 col-md-2">
                         <h5>Quick links</h5>
                         <ul>
                             <li><a href="#">In-Store Pickup</a></li>
@@ -52,30 +52,30 @@
                             <li><a href="#">Store Locator</a></li>
                             <li><a href="#">FAQ</a></li>
                         </ul>
-                    </div>
-                    <div class="col-sm-4 col-md-2">
+                    </div>-->
+                    <div class="col-sm-4 col-md-3">
                         <h5>Order info</h5>
                         <ul>
                             <li><a href="#">Order Status </a></li>
                             <li><a href="#">Payments</a></li>
                             <li><a href="#">Shipping</a></li>
                             <li><a href="#">Returns</a></li>
-                            <li><a href="#">Exchanges</a></li>
-                            <li><a href="#">Order history</a></li>
+                            <!--<li><a href="#">Exchanges</a></li>
+                            <li><a href="#">Order history</a></li>-->
                         </ul>
                     </div>
-                    <div class="col-sm-4 col-md-2">
+                    <div class="col-sm-4 col-md-3">
                         <h5>Customer service</h5>
                         <ul>
                             <li><a href="#">Help Center</a></li>
-                            <li><a href="#">Product Recalls</a></li>
+                            <!--<li><a href="#">Product Recalls</a></li>-->
                             <li><a href="#">Shipping</a></li>
                             <li><a href="#">Feedback</a></li>
-                            <li><a href="#">Store Pickup</a></li>
+                            <!--<li><a href="#">Store Pickup</a></li>-->
                             <li><a href="#">Contact us</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-12 col-md-4">
+                    <div class="col-sm-12 col-md-3">
                         <h5>Sign up for our newsletter</h5>
                         <p><i style="color: #fff;">Add your email address to sign up for our monthly emails and to receive promotional offers.</i></p>
                         <div class="form-group form-newsletter">
