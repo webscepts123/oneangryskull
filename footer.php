0d454820f60a423295ec282b46127270406f444b
webscept_solutions <accounts@webscepts.com>
2023-11-29 02:06:52 +0530

    newchangepage

--- footer.php
+++ footer.php
@@ -39,6 +39,7 @@
                             <li><a href="<?php bloginfo('url'); ?>/women/">Woman</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/accessories/">Accessories</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/sportwear/">Sportwear</a></li>
+                            <li><a href="<?php bloginfo('url'); ?>/faq/">FAQ</a></li>
                         </ul>
                     </div>
                     <!--<div class="col-sm-4 col-md-2">
@@ -57,7 +58,7 @@
                         <ul>
                             <li><a href="<?php bloginfo('url'); ?>/shipping/">Shipping</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/terms-and-conditions/">Terms and Conditions</a></li>
-                            <li><a href="<?php bloginfo('url'); ?>/index.php/terms-and-conditions/">Terms and Conditions</a></li>
+                            <li><a href="<?php bloginfo('url'); ?>/terms-and-conditions/">Terms and Conditions</a></li>
                             <!--<li><a href="#">Exchanges</a></li>
                             <li><a href="#">Order history</a></li>-->
                         </ul>
@@ -66,9 +67,9 @@
                         <h5>Customer service</h5>
                         <ul>
                             <!--<li><a href="#">Product Recalls</a></li>-->
-                            <li><a href="<?php bloginfo('url'); ?>/index.php/faq/">FAQ</a></li>
+                            <li><a href="#">Feedback</a></li>
                             <!--<li><a href="#">Store Pickup</a></li>-->
-                            <li><a href="<?php bloginfo('url'); ?>/index.php/contact-us/">Contact us</a></li>
+                            <li><a href="<?php bloginfo('url'); ?>/contact-us/">Contact us</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-12 col-md-3">
@@ -87,7 +88,7 @@
             <div class="footer-social">
                 <div class="row">
                     <div class="col-sm-6">
-                        <a href="<?php bloginfo('url'); ?>/sitemap/">Sitemap</a> &nbsp; | &nbsp; <a href="<?php bloginfo('url'); ?>/index.php/privacy-policy/">Privacy policy</a>
+                        <a href="<?php bloginfo('url'); ?>/sitemap/">Sitemap</a> &nbsp; | &nbsp; <a href="<?php bloginfo('url'); ?>/privacy-policy/">Privacy policy</a>
                     </div>
                     <div class="col-sm-6 links">
                         <ul>
